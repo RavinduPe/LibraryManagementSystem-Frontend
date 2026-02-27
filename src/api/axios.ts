74272d93d4b08931d20ffe5754af98c3c7b86a7b
RavinduPe <raviduy60@gmail.com>
2026-02-27 22:10:26 +0530

    Add Pinia for state management and implement auth store; remove unused API files

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -1,11 +1,20 @@
 import axios from 'axios';
 import.meta.env.VITE_MY_VAR
+import { useAuthStore } from '@/stores/auth';
 
 const api = axios.create({
     baseURL: import.meta.env.BASEURL || 'http://localhost:8080/api',
     headers: {
         'Content-Type': 'application/json',
     },
+})
+
+api.interceptors.request.use(config => {
+  const auth = useAuthStore()
+  if (auth.token) {
+    config.headers.Authorization = `Bearer ${auth.token}`
+  }
+  return config
 })
 
 export default api;
